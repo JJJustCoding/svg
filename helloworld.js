3f0289fa23c3b252f5f94a949d68601889f4c9bc
温浪 <lang_wen@intsig.net>
2018-02-13 18:26:20 +0800

    js

---
+++ helloworld.js
@@ -0,0 +1,28 @@
+let relationMap = [
+  {
+    type: 'one',
+    relation: [
+      {
+        name: 'Jason',
+        company: '上海合合信息'
+      },
+      {
+        name: '刘大风',
+        position: '销售经理',
+        isContacts: true
+      }
+    ]
+  },
+  {
+    type: 'two',
+    relation: [
+      {
+        name: 'Jason',
+        company: '上海合合信息'
+      },
+      {
+        
+      }
+    ]
+  }
+]
